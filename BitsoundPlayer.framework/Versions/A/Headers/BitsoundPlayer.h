8767145cbcedd84777e9e7e7ce0998bb6c01c1b9
wonje-soundlly <wonje@soundl.ly>
2017-07-28 12:36:23 +0900

    Update API comments

--- BitsoundPlayer.framework/Versions/A/Headers/BitsoundPlayer.h
+++ BitsoundPlayer.framework/Versions/A/Headers/BitsoundPlayer.h
@@ -16,34 +16,39 @@
 @property (nonatomic, weak, nullable) id <BitsoundPlayerDelegate> delegate;
 
 /**
- *  singleton BitsoundReceiver instance를 return한다.
+ *  singleton BitsoundPlayer instance를 return한다.
  */
 + (nonnull BitsoundPlayer *)sharedInstance;
 
 /**
- * 해당 beacon으로 신호 play
- * beacon : play할 beacon id
- * volume : VOL_MAX_DBFS = 0, VOL_MIN_DBFS = -32
+ * 해당 소리 신호(Sound Beacon)를, volume 크기로 play한다.
+ * BitsoundPlayerDelegate protocol method를 통해, play에 대한 이벤트가 전달된다.
+ * beacon : 소리 신호(Sound Beacon) 아이디 (사운들리 Portal에서 발급할 수 있습니다.)
+ * volume : 소리 크기 (최대 0 dbFs ~ 최소 -32 dbFs 설정 가능)
  */
 
 - (void)playWithBeacon:(unsigned int)beacon volume:(int)volume;
 
 /**
- * 해당 beacon으로, duration만큼 신호 play
- * beacon : play할 beacon id
- * volume : VOL_MAX_DBFS = 0, VOL_MIN_DBFS = -32
- * duration : 초단위로, duration 동안 play하다가 stop
+ * 해당 소리 신호(Sound Beacon)를, volume 크기로 duration동안 play하고, stop된다.
+ * BitsoundPlayerDelegate protocol method를 통해, play에 대한 이벤트가 전달된다.
+ * beacon : 소리 신호(Sound Beacon) 아이디 (사운들리 Portal에서 발급할 수 있습니다.)
+ * volume : 소리 크기 (최대 0 dbFs ~ 최소 -32 dbFs 설정 가능)
+ * duration : play 되는 시간으로, 초 단위로 설정 가능
  */
 - (void)playWithBeacon:(unsigned int)beacon volume:(int)volume duration:(NSTimeInterval)duration;
 
 /**
- * play 종료
+ * stop 시킨다.
+ * BitsoundPlayerDelegate protocol method를 통해, stop에 대한 이벤트가 전달된다.
  */
 - (void)stop;
 
 @end
 
-
+/**
+ * BitsoundPlayerDelegate protocol은 소리 신호(Sound Beacon) play/stop시 발생하는, 이벤트 정보 전달해주는 메소드를 정의한다.
+ */
 @protocol BitsoundPlayerDelegate <NSObject>
 
 /**
